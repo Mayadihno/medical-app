a9ad6015edb197dd464bbfdafad32d348c98156c
Mayadihno <mayadihno@gmail.com>
2024-04-22 01:15:23 +0100

    added navbar and hero section

--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Home() {
-  return (
-    <section className="flex text-3xl justify-center items-center min-h-screen">
-      <h2 className=" font-semibold">Medical App</h2>
-    </section>
-  );
-}
